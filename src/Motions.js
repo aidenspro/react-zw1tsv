239911800fe2acf79b7d519324a5f0aca9776d9f
aidenspro <aidenspro@gmail.com>
2022-05-18 11:14:51 -0400

    1 binaries uploaded

--- src/Motions.js
+++ src/Motions.js
@@ -3,17 +3,24 @@ import './style.css';
 
 export default function Motions(props) {
 
+  const [output, setOutput] = useState("➧");
 
+  const addInputs = (motion) => {
+    var currentInputs = (output);
+    currentInputs += motion;
+    setOutput(currentInputs)
+  }
   return (
     <div>
       <div className="motionoptions">
-      <div className="qcf"> </div>
-      <div className="qcb"> </div>
-      <div className="hcf"> </div>
-      <div className="hcb"> </div>
-      <div className="srk"> </div>
-      <div className="arcsys"> </div>
-</div>
+        <div className="qcf" onClick={() => addInputs(" 236") }> </div>
+        <div className="qcb"> </div>
+        <div className="hcf"> </div>
+        <div className="hcb"> </div>
+        <div className="srk"> </div>
+        <div className="arcsys"> </div>
+      </div>
+      <div className="output"> {output} </div>
     </div>
         )
 }
