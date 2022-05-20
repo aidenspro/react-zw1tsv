f6887323d066e931f09d8e7de7ea7c6619743be0
aidenspro <aidenspro@gmail.com>
2022-05-20 10:06:31 -0400

    2 files modified

--- src/Motions.js
+++ src/Motions.js
@@ -15,19 +15,23 @@ export default function Motions(props) {
       <div>
       <div className="motionoptions">
         <div className="qcf" onClick={() => addInputs(" 236") }> </div>
-        <div className="qcb"> </div>
-        <div className="hcf"> </div>
-        <div className="hcb"> </div>
-        <div className="srk"> </div>
-        <div className="arcsys"> </div>
+        <div className="qcb" onClick={() => addInputs(" 214") }> </div>
+        <div className="hcf" onClick={() => addInputs(" 426") }> </div>
+        <div className="hcb" onClick={() => addInputs(" 624") }> </div>
+        <div className="srk" onClick={() => addInputs(" 623") }> </div>
+        <div className="arcsys" onClick={() => addInputs(" 6246") } > </div>
       </div>
-      <div className="motionoptions">
-        
-        <div className="punch"> </div>
-        <div className="kick"> </div>
-        <div className="slash"> </div>
-        <div className="hslash"> </div>
-        <div className="dust"> </div>
+      <div className="buttonoptions">
+        <div className="punch" onClick={() => addInputs("P ") } > </div>
+        <div className="kick" onClick={() => addInputs("K ") } > </div>
+        <div className="slash" onClick={() => addInputs("S ") } > </div>
+        <div className="hslash" onClick={() => addInputs("HS ") } > </div>
+        <div className="dust" onClick={() => addInputs("D ") } > </div>
+      </div>
+
+      <div className="otheroptions">
+        <div className="cancel" onClick={() => addInputs(" x ") } > </div>
+        <div className="link" onClick={() => addInputs(" → ") } > </div>
       </div>
       </div>
         
